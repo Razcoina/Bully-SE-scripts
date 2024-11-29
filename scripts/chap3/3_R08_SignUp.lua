86d4a66cea8680cb2b879d249fcc63f114a5c0e3
Razcoina <90851049+Razcoina@users.noreply.github.com>
2024-11-29 18:14:25 +0000

    Add files via upload

---
+++ scripts/chap3/3_R08_SignUp.lua
@@ -0,0 +1,37 @@
+--[[ Changes to this file:
+	* Modified function main, may require testing
+]]
+
+local missionName
+
+function MissionSetup()
+end
+
+function MissionCleanup()
+end
+
+function main() -- ! Modified
+	local missionName = MissionGetCurrentName()
+	if string.find(missionName, "Rich Area 1") then
+		shared.g3_R08_CurrentRace = 0
+		TextPrint("GOTO_RACERICH1", 3, 1)
+	elseif string.find(missionName, "Rich Area 2") then
+		shared.g3_R08_CurrentRace = 1
+		TextPrint("GOTO_RACERICH2", 3, 1)
+	elseif string.find(missionName, "Business") then
+		shared.g3_R08_CurrentRace = 2
+		TextPrint("GOTO_RACEBUSINESS", 3, 1)
+	elseif string.find(missionName, "Poor") then
+		shared.g3_R08_CurrentRace = 3
+		TextPrint("GOTO_RACEPOOR", 3, 1)
+	elseif string.find(missionName, "School") then
+		shared.g3_R08_CurrentRace = 4
+		TextPrint("GOTO_RACESCHOOL", 3, 1)
+		--[[
+	else
+		TextPrintString("ERROR: Race sign-up failed -- Jak", 3, 2)
+	]] -- Not present in original script
+	end
+	SoundPlayMissionEndMusic(true, 10)
+	MissionSucceed()
+end
