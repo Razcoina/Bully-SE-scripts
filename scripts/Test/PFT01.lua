c4a4ea8451d3066e029e33ba7c6858be32e85908
Razcoina <90851049+Razcoina@users.noreply.github.com>
2024-12-04 16:14:19 +0000

    Add files via upload

---
+++ scripts/Test/PFT01.lua
@@ -0,0 +1,50 @@
+ImportScript("\\Library\\LibTable.lua")
+ImportScript("\\Library\\LibPed.lua")
+local mission_completed = false
+
+function MissionSetup()
+	DATLoad("TFIGHT01.DAT", 2)
+	DATInit()
+	PlayerSetHealth(200)
+	AreaTransitionPoint(22, POINTLIST._TFIGHT01_C)
+	EnemyCreate()
+end
+
+function EnemyCreate()
+	L_PedLoadPoint(nil, {
+		{
+			model = 24,
+			point = POINTLIST._TFIGHT01_NE_01
+		},
+		{
+			model = 27,
+			point = POINTLIST._TFIGHT01_E_01
+		},
+		{
+			model = 26,
+			point = POINTLIST._TFIGHT01_SE_01
+		},
+		{
+			model = 28,
+			point = POINTLIST._TFIGHT01_W_01
+		}
+	})
+end
+
+function MissionCleanup()
+	DATUnload(2)
+end
+
+function main()
+	L_PedExec(nil, PedClearAllWeapons, "id")
+	L_PedExec(nil, PedSetWeaponNow, "id", 303, 1)
+	L_PedExec(nil, PedAttack, "id", gPlayer)
+	while mission_completed == false do
+		if L_PedAllDead() then
+			mission_completed = true
+		end
+		Wait(0)
+	end
+	Wait(3000)
+	MissionSucceed()
+end
